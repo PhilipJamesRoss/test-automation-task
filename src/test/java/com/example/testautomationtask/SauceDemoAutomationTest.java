4168c1319126f8f138b0c40bac799a20410862ce
philip.ross <philip.ross@engineering.digital.dwp.gov.uk>
2023-03-13 17:41:17 +0000

    Add updated code

--- src/test/java/com/example/testautomationtask/SauceDemoAutomationTest.java
+++ src/test/java/com/example/testautomationtask/SauceDemoAutomationTest.java
@@ -4,25 +4,25 @@ import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-public class MainPageTest {
+public class SauceDemoAutomationTest {
     private WebDriver driver;
-    private MainPage mainPage;
+    private SauceDemoPageElements sauceDemoPageElements;
 
     @BeforeEach
     public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "src/utils/drivers/chromedriver");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.get("https://www.jetbrains.com/");
-
-        mainPage = new MainPage(driver);
+        sauceDemoPageElements = new SauceDemoPageElements(driver);
     }
 
     @AfterEach
@@ -30,34 +30,49 @@ public class MainPageTest {
         driver.quit();
     }
 
+    //Automation task
     @Test
-    public void search() {
-        mainPage.searchButton.click();
+    public void addHighestSauceItemToCart() {
+        //1. Navigate to the following URL https://www.saucedemo.com/
+        driver.get(" https://www.saucedemo.com/");
 
-        WebElement searchField = driver.findElement(By.cssSelector("[data-test='search-input']"));
-        searchField.sendKeys("Selenium");
+        //2. Login using the following details(username: standard_user, Password: secret_sauce)
+        sauceDemoPageElements.usernameField.sendKeys("standard_user");
+        sauceDemoPageElements.passwordField.sendKeys("secret_sauce");
+        sauceDemoPageElements.loginButton.click();
+        assertTrue(sauceDemoPageElements.backPackAddToCart.isDisplayed());
 
-        WebElement submitButton = driver.findElement(By.cssSelector("button[data-test='full-search-button']"));
-        submitButton.click();
-
-        WebElement searchPageField = driver.findElement(By.cssSelector("input[data-test='search-input']"));
-        assertEquals("Selenium", searchPageField.getAttribute("value"));
-    }
-
-    @Test
-    public void toolsMenu() {
-        mainPage.toolsMenu.click();
-
-        WebElement menuPopup = driver.findElement(By.cssSelector("div[data-test='main-submenu']"));
-        assertTrue(menuPopup.isDisplayed());
-    }
-
-    @Test
-    public void navigationToAllTools() {
-        mainPage.seeAllToolsButton.click();
-
-        WebElement productsList = driver.findElement(By.id("products-page"));
-        assertTrue(productsList.isDisplayed());
-        assertEquals("All Developer Tools and Products by JetBrains", driver.getTitle());
+        //3. Select the highest price item (Please do not use the “Sort By” option on the page).
+        // Find the price for all items on the page and convert to type double
+        double backPackPrice = Double.parseDouble(sauceDemoPageElements.itemPrices.get(0).getText().substring(1));
+        double bikeLightPrice = Double.parseDouble(sauceDemoPageElements.itemPrices.get(1).getText().substring(1));
+        double boltTShirt = Double.parseDouble(sauceDemoPageElements.itemPrices.get(2).getText().substring(1));
+        double fleeceJacketPrice = Double.parseDouble(sauceDemoPageElements.itemPrices.get(3).getText().substring(1));
+        double onsiePrice = Double.parseDouble(sauceDemoPageElements.itemPrices.get(4).getText().substring(1));
+        double testAllTheThingsTShirt = Double.parseDouble(sauceDemoPageElements.itemPrices.get(5).getText().substring(1));
+        // Create a list of the items prices
+        List<Double> itemList = new ArrayList<>();
+        itemList.add(backPackPrice);
+        itemList.add(bikeLightPrice);
+        itemList.add(boltTShirt);
+        itemList.add(fleeceJacketPrice);
+        itemList.add(onsiePrice);
+        itemList.add(testAllTheThingsTShirt);
+        // Find the highest priced item
+        Double mostExpensiveItem = Collections.max(itemList);
+        //4. Add the selected highest price item to the cart
+        // Add the highest priced item to cart
+        if (mostExpensiveItem == itemList.get(0).doubleValue()) {
+            sauceDemoPageElements.backPackAddToCart.click();
+        } else if (mostExpensiveItem == itemList.get(1).doubleValue()) {
+            sauceDemoPageElements.bikeLightAddToCart.click();
+        } else if (mostExpensiveItem == itemList.get(2).doubleValue()) {
+            sauceDemoPageElements.boltTShirtAddToCart.click();
+        } else if (mostExpensiveItem == itemList.get(3).doubleValue()) {
+            sauceDemoPageElements.fleeceJacketAddToCart.click();
+        } else if (mostExpensiveItem == itemList.get(4).doubleValue()) {
+            sauceDemoPageElements.onesieAddToCart.click();
+        } else
+            sauceDemoPageElements.testAllTheThingsTShirtAddToCart.click();
     }
 }
--- src/test/java/com/example/testautomationtask/SauceDemoPageElements.java
+++ src/test/java/com/example/testautomationtask/SauceDemoPageElements.java
@@ -4,19 +4,42 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.w3c.dom.stylesheets.LinkStyle;
 
-// page_url = https://www.jetbrains.com/
-public class MainPage {
-    @FindBy(css = "a.wt-button_mode_primary")
-    public WebElement seeAllToolsButton;
+import java.util.List;
 
-    @FindBy(xpath = "//div[@data-test='main-menu-item' and @data-test-marker = 'Developer Tools']")
-    public WebElement toolsMenu;
+public class SauceDemoPageElements {
 
-    @FindBy(css = "[data-test='site-header-search-action']")
-    public WebElement searchButton;
+    @FindBy(id = "user-name")
+    public WebElement usernameField;
 
-    public MainPage(WebDriver driver) {
+    @FindBy(id = "password")
+    public WebElement passwordField;
+
+    @FindBy(id = "login-button")
+    public WebElement loginButton;
+
+    @FindBy(className = "inventory_item_price")
+    public List<WebElement> itemPrices;
+
+    @FindBy(id = "add-to-cart-sauce-labs-backpack")
+    public WebElement backPackAddToCart;
+
+    @FindBy(id = "add-to-cart-sauce-labs-bike-light")
+    public WebElement bikeLightAddToCart;
+
+    @FindBy(id = "add-to-cart-sauce-labs-bolt-t-shirt")
+    public WebElement boltTShirtAddToCart;
+
+    @FindBy(id = "add-to-cart-sauce-labs-fleece-jacket")
+    public WebElement fleeceJacketAddToCart;
+
+    @FindBy(id = "add-to-cart-sauce-labs-onesie")
+    public WebElement onesieAddToCart;
+
+    @FindBy(id = "add-to-cart-test.allthethings()-t-shirt-(red)")
+    public WebElement testAllTheThingsTShirtAddToCart;
+    public SauceDemoPageElements(WebDriver driver) {
         PageFactory.initElements(driver, this);
     }
 }
